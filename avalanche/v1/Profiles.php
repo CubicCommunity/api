f3ee482ee1e4be1e76bba54cc052e8c5627e4bc8
Cheeseworks <bluewitherer2422@gmail.com>
2025-07-02 07:53:29 -0500

    aval api

---
+++ avalanche/v1/Profiles.php
@@ -0,0 +1,24 @@
+<?php
+// filepath: public_html/api/projects.php
+header('Content-Type: application/json');
+$data = json_decode(file_get_contents('https://gh.cubicstudios.xyz/WebLPS/data/avalProfiles.json'), true);
+
+if ($data === null) {
+    http_response_code(500);
+    echo json_encode(['error' => 'Failed to retrieve data']);
+    exit;
+} else {
+    unset($data['$schema']);
+
+    if (isset($_GET['id'])) {
+        $id = $_GET['id'];
+        if (isset($data[$id])) {
+            echo json_encode($data[$id]);
+        } else {
+            http_response_code(404);
+            echo json_encode(['error' => 'Profile not found']);
+        }
+    } else {
+        echo json_encode($data);
+    }
+}
