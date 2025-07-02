28b704847f150b35494c2687a8340a8ace21df72
Cheeseworks <bluewitherer2422@gmail.com>
2025-07-02 17:47:08 -0500

    get

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
 header('Content-Type: application/json');
+
+if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
+    http_response_code(405);
+    header('Allow: GET');
+    header('Content-Type: application/json');
+    echo json_encode(['error' => 'Method Not Allowed']);
+    exit;
+}
+
 echo json_encode([
     'service' => 'cubicstudios',
     'status' => 'ok',
