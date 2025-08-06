3e740143676f643d9d5128b3ca9d00ec6c94af6b
Cheeseworks <bluewitherer2422@gmail.com>
2025-08-06 10:14:56 -0500

    webp support

--- avalanche/v1/featured/thumbnail.php
+++ avalanche/v1/featured/thumbnail.php
@@ -9,19 +9,37 @@ header('Content-Type: application/json');
 // The kind of request we're expecting
 $utils->checkMethod($_SERVER['REQUEST_METHOD'], RequestMethod::GET);
 
-// Fetch the remote image
-$image = file_get_contents('https://gh.cubicstudios.xyz/WebLPS/aval-project/thumbnail.png');
-if ($image === false) { // Check if the image was fetched successfully
+$imageData = file_get_contents("https://gh.cubicstudios.xyz/WebLPS/aval-project/thumbnail.png");
+if ($imageData === false) {
     http_response_code(404);
     echo json_encode([
         'error' => 'Image not found',
     ]);
+
     exit;
 }
 
-// Set the correct content type for PNG images
-header('Content-Type: image/png');
+// Try to create an image resource from the data
+$image = imagecreatefromstring($imageData);
+
+if ($image === false) { // Check if the image was fetched successfully
+    http_response_code(404);
+    echo json_encode([
+        'error' => 'Unsupported image format',
+    ]);
+
+    exit;
+}
 
 // Output the image data
 http_response_code(200);
-echo $image;
+
+if (isset($_GET['webp'])) { // Check if request includes WebP parameter
+    // Set the correct content type for WebP images
+    header('Content-Type: image/webp');
+    imagewebp($image);
+} else {
+    // Set the correct content type for JPEG images
+    header('Content-Type: image/jpeg');
+    imagejpeg($image);
+}
